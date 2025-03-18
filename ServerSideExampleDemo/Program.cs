89fa97febc164bd4db1f567c1aef867f4f25edba
Florian Gilde <fgilde@gmail.com>
2025-03-18 22:19:21 +0100

    test

---
+++ ServerSideExampleDemo/Program.cs
@@ -0,0 +1,35 @@
+using MudBlazor.Extensions;
+using ServerSideExampleDemo.Components;
+using ServerSideExampleDemo.Components.Pages;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServicesWithExtensions();
+builder.Services.AddLocalization();
+
+
+var app = builder.Build();
+app.Use(MudExWebApp.MudExMiddleware);
+app.UseRequestLocalization();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
